bfa29a411f1174cd2823137d39615d4b0c518147
Tharindu Theekshana <138811687+ThariTheekshana@users.noreply.github.com>
2025-03-24 21:09:58 +0530

    latest

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-import 'package:nike/routers/router.dart';
-import 'package:nike/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
